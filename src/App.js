088ec249fadc076a8259ed78fcccaafa1f25c3eb
santhu434 <santhoshrajan434@gmail.com>
2024-08-05 19:39:00 +0530

    final work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from "./components/Header";
 import Banner from "./components/Banner"
-import Topcontainer from "./components/Topcontainer/Topcontainer";
 import Skillcontainer from "./components/Skillcontainer/Skillcontainer";
 import Projectcontainer from "./components/Projectcontainer/Projectcontainer";
 import Experiencecontainer from "./components/Experiencecontainer/Experiencecontainer";
